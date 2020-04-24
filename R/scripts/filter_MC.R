2c3827b6e10679667ee86292f344c7fdfb154e6c
Ubuntu <ubuntu@ip-172-31-6-241.us-east-2.compute.internal>
2020-04-24 02:23:26 +0000

    Added sequential filtering

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -22,8 +22,12 @@ option_list = list(
   #' @param -d The death rate
   optparse::make_option(c("-d", "--deathrate"), action="store", default='all', type='character', help="name of the death scenarios to run, or 'all' to run all of them"),
   optparse::make_option(c("-j", "--jobs"), action="store", default="8", type='integer', help="Number of jobs to run in parallel"),
-  optparse::make_option(c("-k", "--index"), action="store", default="1", type='integer', help = "id of this slot")
-
+  optparse::make_option(c("-k", "--simulations_per_slot"), action="store", default=NA, type='integer', help = "number of simulations to run for this slot"),
+  optparse::make_option(c("-n", "--number_of_simulations"), action="store", default="1", type='integer', help = "number of slots to run"),
+  optparse::make_option(c("-i", "--this_slot"), action="store", default="1", type='integer', help = "id of this slot"),
+  optparse::make_option(c("-y", "--python"), action="store", default="python3", type='character', help="path to python executable"),
+  optparse::make_option(c("-r", "--rpath"), action="store", default="Rscript", type = 'character', help = "path to R executable"),
+  optparse::make_option(c("-p", "--pipepath"), action="store", type='character', help="path to the COVIDScenarioPipeline directory", default = "./")
 )
 
 parser=optparse::OptionParser(option_list=option_list)
@@ -47,7 +51,9 @@ geodata <- report.generation::load_geodata_file(
 )
 obs_nodename <- config$spatial_setup$nodenames
 
-nfiles <- config$nsimulations## set to NULL or the actual number of sim files to include for final report
+if(is.na(opt$simulations_per_slot)){
+  opt$simulations_per_slot <- config$filtering$simulations_per_slot
+}
 
 data_path <- config$filtering$data_path
 data_dir <- gsub('[/][^/]*','',data_path)
@@ -55,56 +61,56 @@ if(!dir.exists(data_dir)){
   dir.create(data_dir,recursive=TRUE)
 }
 # Parse jhucsse using covidImportation
-if (!file.exists(data_path)) {
-  case_data_dir <- paste(config$spatial_setup$base_path,config$spatial_setup$setup_name,"case_data", sep = '/')
-  if(!dir.exists(case_data_dir)){
-    dir.create(case_data_dir,recursive=TRUE)
-  }
-  jhucsse_cases <- covidImportation::get_clean_JHUCSSE_data(aggr_level = "UID",
-                                   last_date = as.POSIXct(lubridate::ymd(config$end_date)),
-                                   case_data_dir = case_data_dir,
-                                   save_raw_data=TRUE,
-                                   us_data_only=FALSE) %>%
-                     select(FIPS,Update,Confirmed)
-
-  jhucsse_deaths <- covidImportation::get_clean_JHUCSSE_deaths(aggr_level = "UID", #"source",
-                               last_date = Sys.time(),
-                               case_data_dir = case_data_dir,
-                               save_raw_data=TRUE,
-                               us_data_only=FALSE) %>%
-                     select(FIPS,Update,Deaths)
-
-  jhucsse <- full_join(jhucsse_cases,jhucsse_deaths)
-  jhucsse  <-
-    jhucsse %>%
-    dplyr::mutate(date = lubridate::ymd(Update)) %>%
-    dplyr::filter(FIPS %in% geodata[[obs_nodename]]) %>%
-    dplyr::rename(
-      cumConfirmed = Confirmed,
-      cumDeaths = Deaths
-    ) %>%
-    dplyr::arrange(date)
-  if(any(is.na(jhucsse$cumConfirmed))){
-    jhucsse$cumConfirmed[is.na(jhucsse$cumConfirmed)] <- 0
-  }
-  if(any(is.na(jhucsse$cumDeaths))){
-    jhucsse$cumDeaths[is.na(jhucsse$cumDeaths)] <- 0
-  }
-  jhucsse <- jhucsse %>%
-    dplyr::group_by(FIPS) %>%
-    dplyr::group_modify(
-      function(.x,.y){
-        .x$cumConfirmed = cummax(.x$cumConfirmed)
-        .x$conf_incid = c(.x$cumConfirmed[1],diff(.x$cumConfirmed))
-        .x$cumDeaths = cummax(.x$cumDeaths)
-        .x$death_incid = c(.x$cumDeaths[1],diff(.x$cumDeaths,))
-        return(.x)
-      }
-    )
-  names(jhucsse)[names(jhucsse) == 'FIPS'] <- as.character(obs_nodename)
-  write_csv(jhucsse, data_path)
-  rm(jhucsse)
-}
+###if (!file.exists(data_path)) {
+###  case_data_dir <- paste(config$spatial_setup$base_path,config$spatial_setup$setup_name,"case_data", sep = '/')
+###  if(!dir.exists(case_data_dir)){
+###    dir.create(case_data_dir,recursive=TRUE)
+###  }
+###  jhucsse_cases <- covidImportation::get_clean_JHUCSSE_data(aggr_level = "UID", 
+###                                   last_date = as.POSIXct(lubridate::ymd(config$end_date)),
+###                                   case_data_dir = case_data_dir,
+###                                   save_raw_data=TRUE,
+###                                   us_data_only=FALSE) %>%
+###                     select(FIPS,Update,Confirmed)
+###
+###  jhucsse_deaths <- covidImportation::get_clean_JHUCSSE_deaths(aggr_level = "UID", #"source",
+###                               last_date = Sys.time(),
+###                               case_data_dir = case_data_dir,
+###                               save_raw_data=TRUE,
+###                               us_data_only=FALSE) %>%
+###                     select(FIPS,Update,Deaths)
+###
+###  jhucsse <- full_join(jhucsse_cases,jhucsse_deaths)
+###  jhucsse  <- 
+###    jhucsse %>%
+###    dplyr::mutate(date = lubridate::ymd(Update)) %>%
+###    dplyr::filter(FIPS %in% geodata[[obs_nodename]]) %>%
+###    dplyr::rename(
+###      cumConfirmed = Confirmed,
+###      cumDeaths = Deaths
+###    ) %>%
+###    dplyr::arrange(date)
+###  if(any(is.na(jhucsse$cumConfirmed))){
+###    jhucsse$cumConfirmed[is.na(jhucsse$cumConfirmed)] <- 0
+###  }
+###  if(any(is.na(jhucsse$cumDeaths))){
+###    jhucsse$cumDeaths[is.na(jhucsse$cumDeaths)] <- 0
+###  }
+###  jhucsse <- jhucsse %>%
+###    dplyr::group_by(FIPS) %>% 
+###    dplyr::group_modify(
+###      function(.x,.y){
+###        .x$cumConfirmed = cummax(.x$cumConfirmed)
+###        .x$conf_incid = c(.x$cumConfirmed[1],diff(.x$cumConfirmed))
+###        .x$cumDeaths = cummax(.x$cumDeaths)
+###        .x$death_incid = c(.x$cumDeaths[1],diff(.x$cumDeaths,))
+###        return(.x)
+###      }
+###    )
+###  names(jhucsse)[names(jhucsse) == 'FIPS'] <- as.character(obs_nodename)
+###  write_csv(jhucsse, data_path)
+###  rm(jhucsse)
+###}
 
 # Parse scenarios arguments
 deathrates <- opt$d
@@ -137,9 +143,9 @@ periodAggregate <- function(data, dates, end_date = NULL, period_unit, period_k,
     data <- data[dates <= end_date]
     dates <- dates[dates <= end_date]
   }
-
+  
   xtsobj <- as.xts(zoo(data, dates))
-  stats <- period.apply(xtsobj,
+  stats <- period.apply(xtsobj, 
                         endpoints(xtsobj, on = period_unit, k = period_k),
                         aggregator)
   return(stats)
@@ -152,7 +158,7 @@ getStats <- function(df, time_col, var_col, end_date = NULL, stat_list) {
       aggregator <- match.fun(s$aggregator)
       # Get the time period over whith to apply aggregation
       period_info <- strsplit(s$period, " ")[[1]]
-
+      
       res <- periodAggregate(df[[s[[var_col]]]],
                              df[[time_col]],
                              end_date,
@@ -160,13 +166,50 @@ getStats <- function(df, time_col, var_col, end_date = NULL, stat_list) {
                              period_info[1],
                              aggregator,
                              na.rm = s$remove_na)
-      rc[[stat]] <- res %>%
-        as.data.frame() %>%
-        mutate(date = rownames(.)) %>%
-        set_colnames(c(var_col, "date")) %>%
+      rc[[stat]] <- res %>% 
+        as.data.frame() %>% 
+        mutate(date = rownames(.)) %>% 
+        set_colnames(c(var_col, "date")) %>% 
         select(date, one_of(var_col))
   }
   return(rc)
+}
+
+
+##' Fuction perturbs a seeding file based on a normal
+##' proposal on the start date and
+##' a poisson on the number of cases.
+##'
+##' @param seeding the original seeding
+##' @param sd the standard deviation of the posson
+##'
+##'
+##' @return a pertubed data frame
+##'
+perturb_seeding <- function(seeding,sd) {
+    require(tidyverse)
+    seeding <- seeding%>%
+        group_by(place)%>%
+        mutate(date = date+round(rnorm(1,0,sd)))%>%
+        ungroup()%>%
+        mutate(amount=rpois(length(amount),amount))
+
+    return(seeding)
+
+}
+
+
+
+##'
+##' Function to go through to accept or reject seedings in a block manner based
+##' on a geoid specific likelihood.
+##'
+##'
+##' @param seeding_orig original seeding file.
+##' @param seeding_prop proposal seeding file
+##' @param loc_lls the lik
+iterateAcceptSeeding <- function(x,...){
+  return(x)
 }
 
 
@@ -184,9 +227,9 @@ logLikStat <- function(obs, sim, distr, param, add_one = F) {
   } else if (distr == "nbinom") {
       rc <- dnbinom(obs, sim, k = param[1], log = T)
   } else if (distr == "sqrtnorm") {
-      rc <- dnorm(sqrt(obs), sqrt(sim), sd=sqrt(sim)*param[1], log = T)
-  } else if (distr == "sqrtnorm_scale_sim") ( #param 1 is cov, param 2 is multipler
-      rc <- dnorm(sqrt(obs), sqrt(sim*param[2]), sd=sqrt(sim*param[2])*param[1],log=T)
+      rc <- dnorm(sqrt(obs), sqrt(sim), sd=sqrt(sim)*param[[1]], log = T)
+  } else if (distr == "sqrtnorm_scale_sim") { #param 1 is cov, param 2 is multipler
+      rc <- dnorm(sqrt(obs), sqrt(sim*param[[2]]), sd=sqrt(sim*param[[2]])*param[[1]],log=T)
   } else {
       stop("Invalid stat specified")
   }
@@ -194,19 +237,14 @@ logLikStat <- function(obs, sim, distr, param, add_one = F) {
   return(rc)
 }
 
-iterateAccept <- function(df, ll_col) {
-  ind <- 1
-  lls <- df[[ll_col]]
-  ll_ref <- lls[1]
-  for (i in 2:length(lls)) {
-    ll_new <- lls[i]
-    ll_ratio <- exp(min(c(0, ll_new - ll_ref)))
-    if (ll_ratio >= runif(1)) {
-      ll_ref <- ll_new
-      ind <- i
-    }
+iterateAccept <- function(ll_ref,ll_new,ll_col) {
+  ll_new <- ll_new[[ll_col]]
+  ll_ref <- ll_ref[[ll_col]]
+  ll_ratio <- exp(min(c(0, ll_new - ll_ref)))
+  if (ll_ratio >= runif(1)) {
+    return(TRUE)
   }
-  return(data.frame(ll = ll_ref, ind_accept = ind))
+  return(FALSE)
 }
 
 if(!("obs" %in% ls())){
@@ -223,34 +261,77 @@ data_stats <- lapply(
       "date",
       "data_var",
       stat_list = config$filtering$statistics)
-  }) %>%
+  }) %>% 
     set_names(geonames)
 
 required_packages <- c("dplyr", "magrittr", "xts", "zoo", "stringr")
-# foreach (scenario = scenarios) %:%
-#   foreach(deathrate = deathrates) %do% {
 for(scenario in scenarios) {
   for(deathrate in deathrates) {
-  # profout <- profvis::profvis({
       # Data -------------------------------------------------------------------------
       # Load
+    
+    err <- system(paste(
+      opt$rpath,
+      paste(opt$pipepath,"R","scripts","create_seeding.R", sep='/'),
+      "-c",opt$config
+    ))
+    if(err != 0){quit("no")}
 
-    scenario_files <- list.files(paste0('hospitalization/model_output/',config$name,"_",scenario),full.names=TRUE)
-    scenario_files <- scenario_files[grepl(deathrate,gsub('.*/','',scenario_files))]
+    current_index <- 0
+    current_likelihood <- data.frame()
 
-    cl <- parallel::makeCluster(opt$j)
-    doParallel::registerDoParallel(cl)
-    ll_data <- foreach(file = scenario_files, .packages = required_packages) %dopar% {
-    # ll_data <- list()
-    # for( file in scenario_files) {
+    initial_seeding <- readr::read_csv(config$seeding$lambda_file)
+    for( index in seq_len(opt$simulations_per_slot)) {
+      print(index)
       # Load sims -----------------------------------------------------------
+      
+      current_seeding <- perturb_seeding(initial_seeding,config$seeding$perturbation_sd)
+     
+      file <- paste(
+        'hospitalization',
+        'model_output',
+        paste0(config$name,'_',scenario),
+        paste0(
+          deathrate,
+          '_',
+          "death_death-",
+          sprintf("%09d",opt$simulations_per_slot * (opt$this_slot - 1) + opt$number_of_simulations + index),
+          '.hosp.parquet'
+        ),
+        sep = '/'
+      )
+      print(paste("Reading",file))
 
-      sim_hosp <- report.generation:::read_file_of_type(gsub(".*[.]","",file))(file) %>%
+      ## Generate files
+      err <- system(paste(
+        opt$python,
+        paste(opt$pipepath,"simulate.py", sep='/'),
+        "-j",opt$jobs,
+        "-c",opt$config,
+        "-n",1,
+        "-i",opt$simulations_per_slot * (opt$this_slot - 1) + opt$number_of_simulations + index
+      ))
+      if(err != 0){quit("no")}
+      ## Run hospitalization
+      err <- system(paste(
+        opt$rpath,
+        paste(opt$pipepath,"R","scripts","hosp_run.R", sep='/'),
+        "-j",opt$jobs,
+        "-c",opt$config,
+        "-p",opt$pipepath,
+        "-n",1,
+        "-i",opt$simulations_per_slot * (opt$this_slot - 1) + opt$number_of_simulations + index
+      ))
+      if(err != 0){quit("no")}
+      
+      print(paste("Reading",file))
+
+      sim_hosp <- report.generation:::read_file_of_type(gsub(".*[.]","",file))(file) %>% 
         filter(time <= max(obs$date)) %>%
         select(-date_inds)
-
+      
       log_likelihood_data <- list()
-
+      
       lhs <- unique(sim_hosp[[obs_nodename]])
       rhs <- unique(names(data_stats))
       all_locations <- rhs[rhs %in% lhs]
@@ -260,7 +341,7 @@ for(scenario in scenarios) {
         # This part can be parallelized
         # One scenarios, one pdeath
         if(!('sim_hosp' %in% ls())){
-          sim_hosp <<- report.generation:::read_file_of_type(gsub(".*[.]","",file))(file) %>%
+          sim_hosp <<- report.generation:::read_file_of_type(gsub(".*[.]","",file))(file) %>% 
             filter(time <= max(obs$date)) %>%
             select(-date_inds)
         }
@@ -271,15 +352,15 @@ for(scenario in scenarios) {
           "time",
           "sim_var",
           end_date = max(obs$date[obs[[obs_nodename]] == location]),
-          config$filtering$statistics
+          config$filtering$statistics 
         )
-
-
+          
+        
         # Get observation statistics
         log_likelihood <- list()
         for(var in names(data_stats[[location]])) {
         # log_likelihood <- foreach (var = names(data_stats[[location]]), .combine = sum) %do% {
-
+          
           log_likelihood[[var]] <- logLikStat(
             obs = data_stats[[location]][[var]]$data_var,
             sim = sim_stats[[var]]$sim_var,
@@ -290,7 +371,7 @@ for(scenario in scenarios) {
         # }
         }
         # Compute log-likelihoods
-
+  
         log_likelihood_data[[location]] <- dplyr::tibble(
           ll = sum(unlist(log_likelihood)),
           filename = file,
@@ -300,32 +381,62 @@ for(scenario in scenarios) {
       # }
       }
       rm(sim_hosp)
-
+  
       log_likelihood_data <- log_likelihood_data %>% do.call(what=rbind)
-
+  
       print(log_likelihood_data)
-
+      
       # Compute total loglik for each sim
-      tmp <- log_likelihood_data %>%
-        group_by(filename) %>%
-        summarise(ll = sum(ll, na.rm = T)) %>%
+      likelihood <- log_likelihood_data %>% 
+        summarise(ll = sum(ll, na.rm = T)) %>% 
         mutate(pdeath = deathrate, scenario = scenario)
-    # ll_data[[file]] <- tmp
-    # }
-      tmp
+      ## For logging
+      if(current_index == 0){
+        current_likelihood <- likelihood
+        current_index <- index
+        initial_seeding <- current_seeding
+        next
+      }
+      if(iterateAccept(current_likelihood, likelihood, 'll')){
+        current_index <- index
+        current_likelihood <- likelihood
+      }
+      initial_seeding <- iterateAcceptSeeding(current_seeding,initial_seeding)
+      print(paste("Current index is ",current_index))
     }
-    # })
-    parallel::stopCluster(cl)
-    ll_data <- do.call(ll_data, what=rbind)
 
-    # Acceptance -------------------------------------------------------------------
+    current_file <- paste(
+      'hospitalization',
+      'model_output',
+      paste0(config$name,'_',scenario),
+      paste0(
+        deathrate,
+        '_',
+        "death_death-",
+        sprintf("%09d",opt$simulations_per_slot * (opt$this_slot - 1) + opt$number_of_simulations + current_index),
+        '.hosp.parquet'
+      ),
+      sep = '/'
+    )
 
-    accepted_data <- iterateAccept(ll_data, "ll")
-    accepted_data$filename <- scenario_files[accepted_data$ind_accept]
-    accepted_data$target_filename <- gsub('hosp[.]','filt.',gsub(paste0('0*',accepted_data$ind_accept),sprintf("%09d",opt$index),gsub('^','filtered_',accepted_data$filename)))
-    accepted_data$target_dir <- gsub('/[^/]*$','',accepted_data$target_filename)
-    dir.create(accepted_data$target_dir, recursive=TRUE)
-    file.copy(from=accepted_data$filename,to=accepted_data$target_filename, overwrite=TRUE)
+    target_file <- paste(
+      'hospitalization',
+      'model_output',
+      paste0(config$name,'_',scenario),
+      paste0(
+        deathrate,
+        '_',
+        "death_death-",
+        sprintf("%09d",opt$this_slot),
+        '.hosp.parquet'
+      ),
+      sep = '/'
+    )
+
+    print(paste("Copying",current_file,"to",target_file))
+    target_dir <- gsub('/[^/]*$','',target_file)
+    dir.create(target_dir, recursive=TRUE)
+    file.copy(from=current_file,to=target_file, overwrite=TRUE)
   }
 }
 #}
