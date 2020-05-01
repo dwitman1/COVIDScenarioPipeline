3ff436b6aac7063edf38650bc98340f9548c8672
Sam Shah <sam@skipflag.com>
2020-04-30 20:28:51 -0700

    perf fix for quantile_report_script

--- scripts/QuantileSummarizeGeoidLevel.R
+++ scripts/QuantileSummarizeGeoidLevel.R
@@ -1,7 +1,15 @@
-## First parse the options to the scripts
+
+# library(devtools)
+# devtools::install_github("hadley/dplyr")
+# install.packages("tdigest")
+
+PROBS = c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
+
 suppressMessages({
     library(optparse, quietly=TRUE)
     library(tidyverse, quietly=TRUE)
+    library(tdigest)
+    library(scales)
 })
 
 ##List of specified options
@@ -16,87 +24,52 @@ option_list <- list(
 
 opt_parser <- OptionParser(option_list = option_list, usage="%prog [options] [one or more scenarios]")
 
-## Paerse the
 arguments <- parse_args(opt_parser, positional_arguments=c(1,Inf))
 opt <- arguments$options
 scenarios <- arguments$args
 
-if(is.null(opt$outfile)) {
+if (is.null(opt$outfile)) {
     stop("outfile must be specified")
 }
 
-##Load the geodata file
+doParallel::registerDoParallel(opt$ncores)
 suppressMessages(geodata <- readr::read_csv("data/geodata.csv"))
 
-
-## Register the parallel backend
-doParallel::registerDoParallel(opt$ncores)
-
-
-##Convert times to date objects
 opt$start_date <- as.Date(opt$start_date)
 opt$end_date <- as.Date(opt$end_date)
 
-
-##Per file filtering code
-post_proc <- function(x,geodata,opt) {
-
-
-  x%>%
-    group_by(geoid) %>%
-      mutate(cum_infections=cumsum(incidI)) %>%
-      mutate(cum_death=cumsum(incidD)) %>%
-      ungroup()%>%
-      filter(time>=opt$start_date& time<=opt$end_date)%>%
-      rename(infections=incidI, death=incidD, hosp=incidH)
+post_proc <- function(x, geodata, opt) {
+    x %>%
+        group_by(geoid) %>%
+        mutate(cum_infections=cumsum(incidI)) %>%
+        mutate(cum_death=cumsum(incidD)) %>%
+        ungroup() %>%
+        filter(time >= opt$start_date & time <= opt$end_date) %>%
+        rename(infections=incidI, death=incidD, hosp=incidH)
 }
 
-##Run over scenarios and death rates as appropriate. Note that
-##Final results will average accross whatever is included
-res_geoid <-list()
-for (i in 1:length(scenarios)) {
-    res_geoid[[i]] <- report.generation::load_hosp_sims_filtered(scenarios[i],
-                                                                 name_filter = opt$name_filter,
-                                                                 num_files = opt$nfiles,
-                                                                 post_process = post_proc,
-                                                                 geodata=geodata,
-                                                                 opt=opt)%>%
-        mutate(scenario=scenarios[i])
+res_geoid <- dplyr::bind_rows(purrr::pmap(data.frame(scenario=scenarios), function(scenario) { 
+    report.generation::load_hosp_sims_filtered(scenario,
+                                               name_filter=opt$name_filter,
+                                               num_files=opt$nfiles,
+                                               post_process=post_proc,
+                                               geodata=geodata,
+                                               opt=opt) 
+}))
 
-
+q <- function(col) {
+  try(tquantile(tdigest(col), PROBS), silent=TRUE)
 }
 
-##Put in one data frame
-res_geoid<-dplyr::bind_rows(res_geoid)
-
-##deregister backend
-doParallel::stopImplicitCluster()
-
-
-
-## Extract quantiles
-tmp_col <- function(x, col) {
-    x%>%
-        group_by(time,geoid) %>%
-        summarize(x=list(enframe(quantile(!!sym(col), probs=c(0.01, 0.025,
-                                                              seq(0.05, 0.95, by = 0.05), 0.975, 0.99)),
-                                 "quantile",col))) %>%
-        unnest(x)
-
-}
-
-
-to_save_geo <- inner_join(tmp_col(res_geoid,"hosp_curr"),
-                         tmp_col(res_geoid,"cum_death"))%>%
-    inner_join(tmp_col(res_geoid,"death"))%>%
-    inner_join(tmp_col(res_geoid,"infections"))%>%
-    inner_join(tmp_col(res_geoid,"cum_infections"))%>%
-    inner_join(tmp_col(res_geoid,"hosp"))
-
+to_save_geo <- res_geoid %>%
+    group_by(time, geoid) %>%
+    summarise(
+        quantile=scales::percent(PROBS),
+        hosp_curr=q(hosp_curr),
+        cum_death=q(cum_death),
+        death=q(death),
+        infections=q(infections),
+        cum_infections=q(cum_infections),
+        hosp=q(hosp))
 
 write_csv(to_save_geo, path=opt$outfile)
-
-
-
-
-
