64c3b4d6533d75663519a301235446aa6c7a3344
Sam Shah <sam@skipflag.com>
2020-04-30 20:28:51 -0700

    Performance fix for quantile_report_script
    Rudimentary performance fix for the quantile_report_script. This commit gets that job running albeit using a less than ideal approach for processing this data.

--- scripts/QuantileSummarizeGeoidLevel.R
+++ scripts/QuantileSummarizeGeoidLevel.R
@@ -1,14 +1,25 @@
-## First parse the options to the scripts
+
+# XXX: this branch was in a transitory state before packrat, so these packages need to be installed to run this script:
+# install.packages("tdigest")
+#
+# TODO: update dependencies
+
+PROBS = c(0.01, 0.025, seq(0.05, 0.95, by = 0.05), 0.975, 0.99)
+
 suppressMessages({
     library(optparse, quietly=TRUE)
     library(tidyverse, quietly=TRUE)
+    library(tdigest)
+    library(scales)
+    library(parallel)
+    library(doParallel)
+    library(data.table)
 })
 
-##List of specified options
 option_list <- list(
     make_option("--name_filter", type="character", default="", help="filename filter, usually deaths"),
     make_option("--nfiles", type="numeric", default=NA, help="number of files to load, default is all"),
-    make_option("--ncores", type="numeric", default=6, help="number of cores to use in data load, default =6"),
+    make_option("--ncores", type="numeric", default=parallel::detectCores(), help="number of cores to use in data load, default =6"),
     make_option(c("--outfile","-o"), type="character", default=NULL, help="file to saver output"),
     make_option("--start_date", type="character", default="2020-01-01", help="earliest date to include"),
     make_option("--end_date",  type="character", default="2022-01-01", help="latest date to include")
@@ -16,87 +27,56 @@ option_list <- list(
 
 opt_parser <- OptionParser(option_list = option_list, usage="%prog [options] [one or more scenarios]")
 
-## Paerse the
 arguments <- parse_args(opt_parser, positional_arguments=c(1,Inf))
 opt <- arguments$options
 scenarios <- arguments$args
 
-if(is.null(opt$outfile)) {
+if (is.null(opt$outfile)) {
     stop("outfile must be specified")
 }
 
-##Load the geodata file
+cl = makeCluster(opt$ncores, outfile="")
+doParallel::registerDoParallel(cl)
 suppressMessages(geodata <- readr::read_csv("data/geodata.csv"))
 
-
-## Register the parallel backend
-doParallel::registerDoParallel(opt$ncores)
-
-
-##Convert times to date objects
 opt$start_date <- as.Date(opt$start_date)
 opt$end_date <- as.Date(opt$end_date)
 
-
-##Per file filtering code
-post_proc <- function(x,geodata,opt) {
-
-
-  x%>%
-    group_by(geoid) %>%
-      mutate(cum_infections=cumsum(incidI)) %>%
-      mutate(cum_death=cumsum(incidD)) %>%
-      ungroup()%>%
-      filter(time>=opt$start_date& time<=opt$end_date)%>%
-      rename(infections=incidI, death=incidD, hosp=incidH)
+post_proc <- function(x, geodata, opt) {
+    x %>%
+        group_by(geoid) %>%
+        mutate(cum_infections=cumsum(incidI)) %>%
+        mutate(cum_death=cumsum(incidD)) %>%
+        ungroup() %>%
+        filter(time >= opt$start_date & time <= opt$end_date) %>%
+        rename(infections=incidI, death=incidD, hosp=incidH)
 }
 
-##Run over scenarios and death rates as appropriate. Note that
-##Final results will average accross whatever is included
-res_geoid <-list()
-for (i in 1:length(scenarios)) {
-    res_geoid[[i]] <- report.generation::load_hosp_sims_filtered(scenarios[i],
-                                                                 name_filter = opt$name_filter,
-                                                                 num_files = opt$nfiles,
-                                                                 post_process = post_proc,
-                                                                 geodata=geodata,
-                                                                 opt=opt)%>%
-        mutate(scenario=scenarios[i])
+res_geoid <- data.table::rbindlist(purrr::pmap(data.frame(scenario=scenarios), function(scenario) { 
+    report.generation::load_hosp_sims_filtered(scenario,
+                                               name_filter=opt$name_filter,
+                                               num_files=opt$nfiles,
+                                               post_process=post_proc,
+                                               geodata=geodata,
+                                               opt=opt) 
+}))
 
-
+q <- function(col) {
+  # if col is empty, tquantile fails; in that case, return what quantile() would (all 0's)
+  tryCatch(tquantile(tdigest(col), PROBS), error = function(e) { quantile(col, PROBS) })
 }
 
-##Put in one data frame
-res_geoid<-dplyr::bind_rows(res_geoid)
-
-##deregister backend
-doParallel::stopImplicitCluster()
-
-
-
-## Extract quantiles
-tmp_col <- function(x, col) {
-    x%>%
-        group_by(time,geoid) %>%
-        summarize(x=list(enframe(quantile(!!sym(col), probs=c(0.01, 0.025,
-                                                              seq(0.05, 0.95, by = 0.05), 0.975, 0.99)),
-                                 "quantile",col))) %>%
-        unnest(x)
-
+res_split <- split(res_geoid, res_geoid$time)
+to_save_geo <- foreach(r_split=res_split, .combine=rbind, .packages="data.table", .inorder=FALSE, .multicombine=TRUE, .verbose=TRUE) %dopar% {
+  stopifnot(is.data.table(r_split))
+  r_split[, .(quantile=scales::percent(PROBS),
+              hosp_curr=q(hosp_curr),
+              cum_death=q(cum_death),
+              death=q(death),
+              infections=q(infections),
+              cum_infections=q(cum_infections),
+              hosp=q(hosp)), by=list(time, geoid)]
 }
+data.table::fwrite(to_save_geo, file=opt$outfile)
 
-
-to_save_geo <- inner_join(tmp_col(res_geoid,"hosp_curr"),
-                         tmp_col(res_geoid,"cum_death"))%>%
-    inner_join(tmp_col(res_geoid,"death"))%>%
-    inner_join(tmp_col(res_geoid,"infections"))%>%
-    inner_join(tmp_col(res_geoid,"cum_infections"))%>%
-    inner_join(tmp_col(res_geoid,"hosp"))
-
-
-write_csv(to_save_geo, path=opt$outfile)
-
-
-
-
-
+stopCluster(cl)
